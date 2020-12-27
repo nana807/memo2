23a7dfedd6a32ea59d5140008651bd6caffbb262
nanaha fujisawa <nanaha.fujisawa@gmail.com>
2020-12-27 13:30:28 +0900

    Initial Commit

---
+++ memo2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  memo2
+//
+//  Created by 藤沢七葉 on 2020/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
